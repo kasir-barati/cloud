98bc41e003ac3b4391e02058bd52eee80c9647a7
Kasir Barati <kasir.barati@gmail.com>
2024-11-25 16:21:00 +0100

    chore: wip

--- deploying-exercises/expressjs-cors/backend/env.helper.js
+++ deploying-exercises/expressjs-cors/backend/env.helper.js
@@ -9,10 +9,18 @@ export function loadEnv() {
   });
 }
 export function getEnv() {
+  loadEnv();
+
   const envs = {
     port: process.env.APP_PORT ?? 3000,
     frontendUrl: process.env.FRONTEND_URL,
   };
+
+  console.group();
+  console.log(process.env.FRONTEND_URL);
+  console.log(process.env.APP_PORT);
+  console.dir(envs, { depth: null });
+  console.groupEnd();
 
   for (const key in envs) {
     if (!envs[key]) {
